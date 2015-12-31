dcf1144543402b36e291c1fc32c9f52f8f65e2c6
Markus Felten <Markus.Felten@gmx.de>
2015-12-31 15:45:20 +0100

    test: test for ES6 classes and plain objects

--- tests/simple.js
+++ tests/simple.js
@@ -59,133 +59,165 @@ class StatefullClass extends stm.StateTransitionMixin(BaseClass, actions, 'stopp
   }
 
   toString() {
-    return `sample`;
+    return 'ES6 class';
   }
 }
 
 stm.defineActionMethods(StatefullClass.prototype, actions, true);
 
-describe('states', function () {
-  describe('static', function () {
-    const o = new StatefullClass(10, false);
+describe('ES6 class', function () {
+  checks((timeout, fail) => new StatefullClass(timeout, fail));
+});
 
-    it('has initial state', function () {
-      o.state = 'stopped';
-      assert.equal(o.state, 'stopped');
-    });
+describe('plain object', function () {
+  checks((startTime, shouldReject) => {
+    const o = {
+      toString() {
+          return "plain object";
+        },
+        _start() {
+          if (shouldReject) return Promise.reject(new Error("always reject"));
 
-    it('has action methods', function () {
-      assert.isDefined(o.stop());
-      assert.isDefined(o.start());
-      assert.isDefined(o._start());
-      assert.isDefined(o._stop());
-    });
+          return new Promise((f, r) => {
+            setTimeout(() => {
+              if (shouldReject) {
+                r(Promise.reject(new Error("always reject")));
+              } else {
+                f(this);
+              }
+            }, startTime);
+          });
+        }
+    };
+    stm.defineActionMethods(o, actions, true);
+    stm.defineStateTransitionProperties(o, actions, "stopped");
 
-    it('defined methods are enumerable', function () {
-      const assigend = Object.assign({}, StatefullClass.prototype);
-      assert.isDefined(assigend.start);
-    });
+    return o;
   });
+});
 
-  describe('start-stop', function () {
-    const o = new StatefullClass(10, false);
+function checks(factory) {
+  describe('states', function () {
+    describe('static', function () {
+      const o = factory(10, false);
 
-    it('can be started', function (done) {
+      it('has initial state', function () {
+        o.state = 'stopped';
+        assert.equal(o.state, 'stopped');
+      });
+
+      it('has action methods', function () {
+        assert.isDefined(o.stop());
+        assert.isDefined(o.start());
+        assert.isDefined(o._start());
+        assert.isDefined(o._stop());
+      });
+
+      it('defined methods are enumerable', function () {
+        const assigend = Object.assign({}, StatefullClass.prototype);
+        assert.isDefined(assigend.start);
+      });
+    });
+
+    describe('start-stop', function () {
+      const o = factory(10, false);
+
+      it('can be started', function (done) {
+        o.start().then(() => {
+          assert.equal(o.state, 'running');
+          done();
+        }, done);
+      });
+
+      it('can be started while running', function (done) {
+        o.start().then(() => {
+          assert.equal(o.state, 'running');
+          done();
+        }, done);
+      });
+
+      it('and stoped', function (done) {
+        o.stop().then(() => {
+          assert.equal(o.state, 'stopped');
+          done();
+        }, done);
+      });
+    });
+
+    it('can be started while starting', function (done) {
+      const o = factory(10, false);
+
+      o.start().then(() => {});
+
+      assert.equal(o.state, 'starting');
+
       o.start().then(() => {
         assert.equal(o.state, 'running');
         done();
-      }, done);
+      }, done).catch(done);
     });
 
-    it('can be started while running', function (done) {
-      o.start().then(() => {
-        assert.equal(o.state, 'running');
-        done();
-      }, done);
-    });
+    it('can be stopped while starting', function (done) {
+      const o = factory(100, false);
 
-    it('and stoped', function (done) {
+      o.start().then(() => {});
+
+      assert.equal(o.state, 'starting');
+
       o.stop().then(() => {
         assert.equal(o.state, 'stopped');
         done();
-      }, done);
+      }, done).catch(done);
     });
-  });
 
-  it('can be started while starting', function (done) {
-    const o = new StatefullClass(10, false);
+    describe('failures', function () {
+      it('illegal transition', function (done) {
+        const o = factory(0, false);
+        try {
+          o.swim().then(() => {
+            console.log(`swimming ?`);
+          }).catch(e => {
+            //console.log(`swimming failed: ${e}`);
+            assert.ok(o.state);
+            done();
+          });
+        } catch (e) {
+          console.log(`error: ${e}`);
+          done(e);
+        }
+      });
 
-    o.start().then(() => {});
-
-    assert.equal(o.state, 'starting');
-
-    o.start().then(() => {
-      assert.equal(o.state, 'running');
-      done();
-    }, done).catch(done);
-  });
-
-  it('can be stopped while starting', function (done) {
-    const o = new StatefullClass(100, false);
-
-    o.start().then(() => {});
-
-    assert.equal(o.state, 'starting');
-
-    o.stop().then(() => {
-      assert.equal(o.state, 'stopped');
-      done();
-    }, done).catch(done);
-  });
-
-  describe('failures', function () {
-    it('illegal transition', function (done) {
-      const o = new StatefullClass(0, false);
-      try {
-        o.swim().then(() => {
-          console.log(`swimming ?`);
-        }).catch(e => {
-          console.log(`swimming failed: ${e}`);
-          assert.ok(o.state);
+      it('handle timeout while starting', function (done) {
+        const o = factory(1000, false);
+        o.start().then(() => {}).catch(e => {
+          assert.equal(o.state, 'failed');
           done();
         });
-      } catch (e) {
-        console.log(`error: ${e}`);
-        done(e);
-      }
-    });
-
-    it('handle timeout while starting', function (done) {
-      const o = new StatefullClass(1000, false);
-      o.start().then(() => {}).catch(e => {
-        assert.equal(o.state, 'failed');
-        done();
       });
-    });
 
-    it('handle failure while starting without timeout guard', function (done) {
-      const o = new StatefullClass(0, true);
+      it('handle failure while starting without timeout guard', function (done) {
+        const o = factory(0, true);
 
-      o.start().then((f, r) => {
-        console.log(`${f} ${r}`);
-      }).catch(e => {
-        //console.log(`catch: ${e}`);
-        assert.equal(o.state, 'failed');
-        done();
+        o.start().then((f, r) => {
+          console.log(`${f} ${r}`);
+        }).catch(e => {
+          //console.log(`catch: ${e}`);
+          assert.equal(o.state, 'failed');
+          done();
+        });
       });
-    });
 
-    it('handle failure while starting with timeout guard', function (done) {
-      const o = new StatefullClass(10, true);
+      it('handle failure while starting with timeout guard', function (done) {
+        const o = factory(10, true);
 
-      o.start().then((f, r) => {
-        console.log(`${f} ${r}`);
-      }).catch(e => {
-        //console.log(`catch: ${e}`);
-        assert.equal(o.state, 'failed');
-        done();
+        o.start().then((f, r) => {
+          console.log(`${f} ${r}`);
+        }).catch(e => {
+          //console.log(`catch: ${e}`);
+          assert.equal(o.state, 'failed');
+          done();
+        });
       });
     });
   });
-
-});
+}
