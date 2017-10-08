9a3fa6967b62ab11f76a7d1a4a590c71ea5e6160
Markus Felten <Markus.Felten@gmx.de>
2017-10-08 11:41:11 +0200

    chore(rollup): remove rollup-plugin-babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
 export default {
@@ -6,15 +5,6 @@ export default {
     file: pkg.main,
     format: 'cjs'
   },
-
-  plugins: [
-    babel({
-      babelrc: false,
-      presets: ['stage-3'],
-      plugins: [],
-      exclude: 'node_modules/**'
-    })
-  ],
 
   input: pkg.module
 };
--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,16 +1,6 @@
-import babel from 'rollup-plugin-babel';
-
 export default {
   input: 'tests/simple-test.js',
   external: ['ava'],
-
-  plugins: [
-    babel({
-      babelrc: false,
-      presets: ['stage-3'],
-      exclude: 'node_modules/**'
-    })
-  ],
 
   output: {
     file: 'build/simple-test.js',
