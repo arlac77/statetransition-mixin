ee5b878955dd238393fe26b78c1048bfb889bd03
Markus Felten <Markus.Felten@gmx.de>
2019-11-29 16:53:37 +0100

    fix: remove superfluous async

--- src/statetransition-mixin.mjs
+++ src/statetransition-mixin.mjs
@@ -197,7 +197,7 @@ export function StateTransitionMixin(superclass, actions, initialState) {
 function rejectUnlessResolvedWithin(promise, timeout, name) {
   if (timeout === 0) return promise;
 
-  return new Promise(async (resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const th = setTimeout(
       () =>
         reject(new Error(`${name} request not resolved within ${timeout}ms`)),
